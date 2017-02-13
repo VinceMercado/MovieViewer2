52b68a10be6dc05a51be6c0736d68e2b78ba298d
VinceMercado <Vinceisdumbcute@vinces-macbook-pro.local>
2017-02-12 22:53:12 -0500

    Update

---
+++ MovieViewer3/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  MovieViewer3
+//
+//  Created by Vince  on 2/10/17.
+//  Copyright © 2017 Vince Mercado. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+   
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-MovieViewer3/Pods-MovieViewer3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieViewer3 : NSObject
+@end
+@implementation PodsDummy_Pods_MovieViewer3
+@end
